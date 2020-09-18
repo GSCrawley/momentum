5a372bd65c6b67d6e9334401d0a2acc86105e05b
GSCrawley <gscrawley@gmail.com>
2020-09-18 11:11:42 -0700

    added input fields

--- momentum.py
+++ momentum.py
@@ -1,3 +1,6 @@
+mass_input = input("Enter your object's mass: ")
+vel_input = input("Enter your object's velocity: ")
+
 class PhysObj:
     def __init__(self, mass, vel):
         """Initialize and set variables"""
@@ -7,3 +10,5 @@ class PhysObj:
     def collide(self, obj2):
         """Calculate the collision with another object"""
         pass
+
+
